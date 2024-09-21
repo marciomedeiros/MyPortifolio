2fb4cefc6f455865307d1845630832199dcdfa59
marciomedeiros <marcio.m.medeiros@gmail.com>
2024-09-21 08:07:39 -0300

    [INIT] initial commit

---
+++ test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["myportifolio/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"myportifolio/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
